e7882fd512adbdaa62aa1274ad821e79948e3f61
Dusuna <94776135+dusunax@users.noreply.github.com>
2022-07-22 23:14:51 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,61 @@
-import './App.css';
-import Week2 from './templates/Week2';
+import React from "react";
+import logo from "./logo.svg";
+// BucketList 컴포넌트를 import 해옵니다.
+// import [컴포넌트 명] from [컴포넌트가 있는 파일경로];
+import BucketList from "./templates/BucketList";
+import styled from "styled-components";
 
-function App() {
-  let arr=[0, 1, 2]
-  return (
-    <div className="App">
-      <Week2 list={ arr }/>
-    </div>
-  );
+// 클래스형 컴포넌트는 이렇게 생겼습니다!
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        // App 컴포넌트의 state를 정의해줍니다.
+        this.state = {
+            list: ["영화관 가기", "매일 책읽기", "수영 배우기"],
+        };
+        this.text = React.createRef();
+    }
+
+componentDidMount(){
+    console.log(this.text);
+    
 }
+
+// 랜더 함수 안에 리액트 엘리먼트를 넣어줍니다!
+render() {
+    return (
+            <div className="App">
+                <Container>
+                    <Title >내 버킷리스트</Title>
+                    <Line/>
+                    <BucketList list={this.state.list} />
+                </Container>
+                <div>
+                    <input type="text" ref={this.text}/>
+                </div>
+            </div>
+        );
+    }
+}
+
+const Container = styled.div`
+max-width: 350px;
+min-height: 80vh;
+background-color: #fff;
+padding: 16px;
+margin: 20px auto;
+border-radius: 5px;
+border: 1px solid #ddd;
+`;
+
+const Title = styled.h1`
+color: slateblue;
+text-align: center;
+`;
+
+const Line = styled.hr`
+margin: 16px 0px;
+border: 1px dotted #ddd;
+`;
 
 export default App;
